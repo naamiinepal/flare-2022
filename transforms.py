c00c6a40a3131721a6620aabb26abf4e5b0dc806
Rabin Adhikari <rabin.adk1@gmail.com>
2022-06-04 10:21:44 +0545

    Move transform functions into the same class

--- transforms.py
+++ transforms.py
@@ -5,101 +5,6 @@ from monai.transforms.utils import generate_spatial_bounding_box
 from skimage.transform import resize
 
 
-def resample_image(image, shape, anisotrophy_flag):
-    resized_channels = []
-    if anisotrophy_flag:
-        for image_c in image:
-            resized_slices = []
-            for i in range(image_c.shape[-1]):
-                image_c_2d_slice = image_c[:, :, i]
-                image_c_2d_slice = resize(
-                    image_c_2d_slice,
-                    shape[:-1],
-                    order=3,
-                    mode="edge",
-                    cval=0,
-                    clip=True,
-                    anti_aliasing=False,
-                )
-                resized_slices.append(image_c_2d_slice)
-            resized = np.stack(resized_slices, axis=-1)
-            resized = resize(
-                resized,
-                shape,
-                order=0,
-                mode="constant",
-                cval=0,
-                clip=True,
-                anti_aliasing=False,
-            )
-            resized_channels.append(resized)
-    else:
-        for image_c in image:
-            resized = resize(
-                image_c,
-                shape,
-                order=3,
-                mode="edge",
-                cval=0,
-                clip=True,
-                anti_aliasing=False,
-            )
-            resized_channels.append(resized)
-    resized = np.stack(resized_channels, axis=0)
-    return resized
-
-
-def resample_label(label, shape, anisotrophy_flag):
-    reshaped = np.zeros(shape, dtype=np.uint8)
-    n_class = np.max(label)
-    if anisotrophy_flag:
-        shape_2d = shape[:-1]
-        depth = label.shape[-1]
-        reshaped_2d = np.zeros((*shape_2d, depth), dtype=np.uint8)
-
-        for class_ in range(1, int(n_class) + 1):
-            for depth_ in range(depth):
-                mask = label[0, :, :, depth_] == class_
-                resized_2d = resize(
-                    mask.astype(float),
-                    shape_2d,
-                    order=1,
-                    mode="edge",
-                    cval=0,
-                    clip=True,
-                    anti_aliasing=False,
-                )
-                reshaped_2d[:, :, depth_][resized_2d >= 0.5] = class_
-        for class_ in range(1, int(n_class) + 1):
-            mask = reshaped_2d == class_
-            resized = resize(
-                mask.astype(float),
-                shape,
-                order=0,
-                mode="constant",
-                cval=0,
-                clip=True,
-                anti_aliasing=False,
-            )
-            reshaped[resized >= 0.5] = class_
-    else:
-        for class_ in range(1, int(n_class) + 1):
-            mask = label[0] == class_
-            resized = resize(
-                mask.astype(float),
-                shape,
-                order=1,
-                mode="edge",
-                cval=0,
-                clip=True,
-                anti_aliasing=False,
-            )
-            reshaped[resized >= 0.5] = class_
-
-    reshaped = np.expand_dims(reshaped, 0)
-    return reshaped
-
-
 class PreprocessAnisotropic(MapTransform):
     """
     This transform class takes NNUNet's preprocessing method for reference.
@@ -126,19 +31,115 @@ class PreprocessAnisotropic(MapTransform):
         self.training = False
         self.crop_foreg = CropForegroundd(keys=["image", "label"], source_key="image")
         self.normalize_intensity = NormalizeIntensity(nonzero=True, channel_wise=True)
-        if model_mode in ["fit"]:
-            self.training = True
+
+        self.training = model_mode == "fit"
 
     def calculate_new_shape(self, spacing, shape):
         spacing_ratio = np.array(spacing) / np.array(self.target_spacing)
         new_shape = (spacing_ratio * np.array(shape)).astype(int).tolist()
         return new_shape
 
-    def check_anisotrophy(self, spacing):
-        def check(spacing):
-            return np.max(spacing) / np.min(spacing) >= 3
+    @staticmethod
+    def check(spacing):
+        return np.max(spacing) / np.min(spacing) >= 3
 
+    def check_anisotrophy(self, spacing):
         return check(spacing) or check(self.target_spacing)
+
+    @staticmethod
+    def resample_image(image, shape, anisotrophy_flag):
+        resized_channels = []
+        if anisotrophy_flag:
+            for image_c in image:
+                resized_slices = []
+                for i in range(image_c.shape[-1]):
+                    image_c_2d_slice = image_c[:, :, i]
+                    image_c_2d_slice = resize(
+                        image_c_2d_slice,
+                        shape[:-1],
+                        order=3,
+                        mode="edge",
+                        cval=0,
+                        clip=True,
+                        anti_aliasing=False,
+                    )
+                    resized_slices.append(image_c_2d_slice)
+                resized = np.stack(resized_slices, axis=-1)
+                resized = resize(
+                    resized,
+                    shape,
+                    order=0,
+                    mode="constant",
+                    cval=0,
+                    clip=True,
+                    anti_aliasing=False,
+                )
+                resized_channels.append(resized)
+        else:
+            for image_c in image:
+                resized = resize(
+                    image_c,
+                    shape,
+                    order=3,
+                    mode="edge",
+                    cval=0,
+                    clip=True,
+                    anti_aliasing=False,
+                )
+                resized_channels.append(resized)
+        resized = np.stack(resized_channels, axis=0)
+        return resized
+
+    @staticmethod
+    def resample_label(label, shape, anisotrophy_flag):
+        reshaped = np.zeros(shape, dtype=np.uint8)
+        n_class = np.max(label)
+        if anisotrophy_flag:
+            shape_2d = shape[:-1]
+            depth = label.shape[-1]
+            reshaped_2d = np.zeros((*shape_2d, depth), dtype=np.uint8)
+
+            for class_ in range(1, int(n_class) + 1):
+                for depth_ in range(depth):
+                    mask = label[0, :, :, depth_] == class_
+                    resized_2d = resize(
+                        mask.astype(float),
+                        shape_2d,
+                        order=1,
+                        mode="edge",
+                        cval=0,
+                        clip=True,
+                        anti_aliasing=False,
+                    )
+                    reshaped_2d[:, :, depth_][resized_2d >= 0.5] = class_
+            for class_ in range(1, int(n_class) + 1):
+                mask = reshaped_2d == class_
+                resized = resize(
+                    mask.astype(float),
+                    shape,
+                    order=0,
+                    mode="constant",
+                    cval=0,
+                    clip=True,
+                    anti_aliasing=False,
+                )
+                reshaped[resized >= 0.5] = class_
+        else:
+            for class_ in range(1, int(n_class) + 1):
+                mask = label[0] == class_
+                resized = resize(
+                    mask.astype(float),
+                    shape,
+                    order=1,
+                    mode="edge",
+                    cval=0,
+                    clip=True,
+                    anti_aliasing=False,
+                )
+                reshaped[resized >= 0.5] = class_
+
+        reshaped = np.expand_dims(reshaped, 0)
+        return reshaped
 
     def __call__(self, data):
         # load data
@@ -170,9 +171,9 @@ class PreprocessAnisotropic(MapTransform):
             resample_flag = True
             resample_shape = self.calculate_new_shape(image_spacings, original_shape)
             anisotrophy_flag = self.check_anisotrophy(image_spacings)
-            image = resample_image(image, resample_shape, anisotrophy_flag)
+            image = self.resample_image(image, resample_shape, anisotrophy_flag)
             if self.training:
-                label = resample_label(label, resample_shape, anisotrophy_flag)
+                label = self.resample_label(label, resample_shape, anisotrophy_flag)
 
         d["resample_flag"] = resample_flag
         d["anisotrophy_flag"] = anisotrophy_flag
