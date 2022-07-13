b694d7b339a729f3944c940e4dd63248ab9fb3f6
mahesh shakya <mahesh.shakya@naamii.org.np>
2022-07-13 11:44:37 +0545

    template matching for abdomen region - baseline commit

---
+++ template_matching/template_matching_longitudinal_correlation.py
@@ -0,0 +1,148 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+
+from pathlib import Path
+import SimpleITK as sitk
+import numpy as np
+
+import template_matching_utils
+
+
+def RegionExtractByTemplateMatching(image: sitk.Image, template_img: sitk.Image):
+    """returns a portion of the "image" that closely matches the "template_img" along with the extent(bounds) of the "image" from which the portion was extracted
+
+    Args:
+        image (sitk.Image): _description_
+        template_img (sitk.Image): _description_
+    Returns:
+        ROI Image (sitk.Image): Region-of-Interest that matches the template image
+    """
+    p_image, p_template = preprocess(image, template_img)
+    outTx = obtain_longitudinal_translation_params(p_image, p_template)
+    print(outTx.GetParameters())
+
+
+def preprocess(
+    image: sitk.Image,
+    template_img: sitk.Image,
+):
+    """bring the template image into same physical space and orientation as the source image
+
+    - zero-out origins
+    - reorient images and template into CANONICAL orientation 
+    - match spacing (change the spacing of the template image)
+        itk image registration already takes this into account, so optional?
+    - use projections if required
+
+
+    Args:
+        image (sitk.Image): _description_
+        template_img (sitk.Image): _description_
+    """
+    # reset origin
+    new_origin = (0,) * image.GetDimension()
+    image.SetOrigin(new_origin)
+    template_img.SetOrigin(new_origin)
+
+    # change template orientation
+    CANONICAL_ORIENTATION = 'RAI'
+    image = sitk.DICOMOrient(image,CANONICAL_ORIENTATION)
+    template_img = sitk.DICOMOrient(template_img, CANONICAL_ORIENTATION)
+
+    # change spacing of template to match the target image
+    template_img = template_matching_utils.match_spacing(image, template_img)
+
+    image_proj = template_matching_utils.generate_projections(image)
+    template_proj = template_matching_utils.generate_projections(template_img)
+
+    SAGITTAL_DIM = template_matching_utils.get_saggital_dim(CANONICAL_ORIENTATION)
+
+    return image_proj[SAGITTAL_DIM], template_proj[SAGITTAL_DIM]
+
+
+def obtain_longitudinal_translation_params(
+    image: sitk.Image, template_img: sitk.Image,
+    GRID_SAMPLING=20,
+    DEBUG_PLOT=True
+):
+    """return translation transformation parameters
+
+    Args:
+        image (sitk.Image): _description_
+        template_img (sitk.Image): _description_
+        GRID_SAMPLING (int): step size in pixel units for the sliding window shift along longitudinal dimension
+    """
+    assert (
+        image.GetDimension() == 2 and template_img.GetDimension()
+    ), f"Images should be 2D but got {image.GetDimension()}"
+    xs, YS = image.GetSize()
+    xt, YT = template_img.GetSize()
+
+    if YT >= YS:
+        # template is bigger than source, do not need to traverse the longitudinal dimension
+        outTx = sitk.TranslationTransform(2, (0, 0))
+        return outTx
+
+    R = sitk.ImageRegistrationMethod()
+    R.SetOptimizerScales((1, 1))
+    R.SetInterpolator(sitk.sitkLinear)
+    R.SetMetricAsCorrelation()
+
+    def command_iteration(method):
+        if method.GetOptimizerIteration() == 0:
+            print("Scales: ", method.GetOptimizerScales())
+        print(
+            "{0:3} = {1:7.5f} : {2}".format(
+                method.GetOptimizerIteration(),
+                method.GetMetricValue(),
+                method.GetOptimizerPosition(),
+            )
+        )
+
+    R.AddCommand(sitk.sitkIterationEvent, lambda: command_iteration(R))
+
+    # slide the template image through the image height(longitudinal dim)
+    # until we go past the image boundary
+    # generate translation parameters from 0 to image height until template overlap does not go out of bounds
+    # YS is the longitudinal dimension of image
+    # YT is the longitudinal dimension of template
+    # we traverse in step size GRID_SAMPLING from 0 to YS(minus the template height)
+    transform_list = [(0, index) for index in range(0, YS - YT, GRID_SAMPLING)]
+    correlations_history = []
+    for translation_param in transform_list:
+        R.SetInitialTransform(sitk.TranslationTransform(2, translation_param))
+        metric = -R.MetricEvaluate(image, template_img)
+        correlations_history.append(metric)
+    optimal_translation_param = sitk.TranslationTransform(
+        2, transform_list[np.argmax(correlations_history)]
+    )
+    if DEBUG_PLOT:
+        template_matching_utils.debug_plot(image,template_img,
+        optimal_translation_param,transform_list,correlations_history)
+    return optimal_translation_param
+
+
+if __name__ == "__main__":
+
+    DEFAULT_ORIENTATION = "RAI"
+    source_image_filename = (
+        # "/mnt/HDD2/flare2022/datasets/FLARE2022/Validation/FLARETs_0050_0000.nii.gz"
+        "/mnt/HDD2/flare2022/datasets/FLARE2022/Training/Unlabeled/Case_00471_0000.nii.gz"
+    )
+
+    for fixed_image_filename in Path(source_image_filename).parent.rglob(
+        "*_0000.nii.gz"
+    ):
+
+        fixed_image_filename = str(fixed_image_filename)
+        moving_image_filename = "/mnt/HDD2/flare2022/datasets/FLARE2022/Training/FLARE22_LabeledCase50/images/FLARE22_Tr_0045_0000.nii.gz"
+
+        # # Load Data
+        try:
+            fixed_image = sitk.ReadImage(fixed_image_filename, sitk.sitkInt16)
+            moving_image = sitk.ReadImage(moving_image_filename, sitk.sitkInt16)
+
+            outTx = RegionExtractByTemplateMatching(fixed_image, moving_image)
+        except RuntimeError:
+            pass
---
+++ template_matching/template_matching_utils.py
@@ -0,0 +1,132 @@
+import os
+import SimpleITK as sitk
+from matplotlib import image
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.patches as patches
+from pathlib2 import Path
+
+def plot_hist(image:sitk.Image):
+    fig = plt.figure()
+    plt.hist(sitk.GetArrayViewFromImage(image).flatten(),bins=100)
+    plt.show()
+
+def generate_projections(image:sitk.Image):
+    assert image.GetDimension() > 2, 'Image should have more than 2 dimension'
+    projection_list = []
+    for i in range(image.GetDimension()):
+        p = extract_slice_helper(sitk.MeanProjection(image,i),dim=i,index=0)
+        projection_list.append(p)
+    return projection_list
+                              
+def generate_random_str(length=5):
+  alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+  return ''.join(np.random.choice(list(alphabet),length,replace=True))
+
+def get_saggital_dim(orientation:str='LPS'):
+  for i,direction in enumerate(list(orientation)):
+    if direction == 'L' or direction == 'R':
+      return i
+  raise ValueError(f'orientation string {orientation} is invalid')
+
+def get_axial_dim(orientation:str='LPS'):
+  for i,direction in enumerate(list(orientation)):
+    if direction == 'A' or direction == 'P':
+      return i
+  raise ValueError(f'orientation string {orientation} is invalid')
+
+def get_longitudinal_dim(orientation:str='LPS'):
+  for i,direction in enumerate(list(orientation)):
+    if direction == 'S' or direction == 'I':
+      return i
+  raise ValueError(f'orientation string {orientation} is invalid')
+
+def debug_plot(image_slice:sitk.Image, template_slice:sitk.Image, outTx:sitk.sitkTranslation,transform_list,correlations):
+  if image_slice.GetDimension() == 3:
+    axial_dim = get_saggital_dim(sitk.DICOMOrientImageFilter_GetOrientationFromDirectionCosines(image_slice.GetDirection()))
+    axial_dim_size = image_slice.GetSize()[axial_dim]
+    image_slice = extract_slice_helper(image_slice,dim=axial_dim,index=axial_dim_size//2)
+  if template_slice.GetDimension() == 3:
+    axial_dim = get_saggital_dim(sitk.DICOMOrientImageFilter_GetOrientationFromDirectionCosines(template_slice.GetDirection()))
+    axial_dim_size = image_slice.GetSize()[axial_dim]
+    template_slice = extract_slice_helper(template_slice,dim=axial_dim,index=axial_dim_size//2)
+
+  fig = plt.figure(figsize=(10,10))
+  ax = plt.subplot(1,3,1)
+  plt.imshow(sitk.GetArrayViewFromImage(template_slice),cmap='gray')
+
+
+  ax2 = plt.subplot(1,3,2)
+  plt.imshow(sitk.GetArrayViewFromImage(image_slice),cmap='gray')
+  # Create a Rectangle patch
+  rect = patches.Rectangle(outTx.GetParameters(), *template_slice.GetSize(), linewidth=1, edgecolor='r', facecolor='none',
+                          clip_on=False)
+
+  # Add the patch to the Axes
+  ax2.add_patch(rect)
+
+  ax3 = plt.subplot(1,3,3)
+  ax3.sharey(ax)
+  ax3.plot(correlations,np.array([y for x,y in transform_list]))
+
+  plt.tight_layout()
+  out_filepath = 'template_matching/images/'+generate_random_str()+'.png' 
+  os.makedirs(str(Path(out_filepath).parent),exist_ok=True)
+  print(f'Saving debug plot to {out_filepath}')
+  plt.savefig(out_filepath)
+  # plt.show()  
+  
+def extract_slice_helper(image:sitk.Image,dim=0,index=0):
+    size = list(image.GetSize())
+    size[dim] = 0
+    slice_index = [0,]*len(size)
+    slice_index[dim] = index
+    return sitk.Extract(image,size,slice_index)
+
+def match_spacing(reference: sitk.Image, image: sitk.Image, is_label=False):
+    """return a new image (with content same as image)
+    which matches the reference image spacing
+
+    Args:
+        image (sitk.Image): _description_
+        reference (sitk.Image): _description_
+        is_label (bool, optional): _description_. Defaults to False.
+
+    Returns:
+        _type_: _description_
+    """
+    assert image.GetDimension() == reference.GetDimension(
+    ), f'Image and reference image should have same dimensions, got {image.GetDimension()} and {reference.GetDimension()}'
+
+    ts = reference.GetSpacing()
+    im_sp = image.GetSpacing()
+    new_spacing = (im_sp[0],im_sp[1],ts[2])
+    im_sz = image.GetSize()
+
+    # expand or contract the image size as required
+    out_sz = []
+    for i in range(image.GetDimension()):
+        out_sz.append(
+            int(np.round(im_sz[i] * (im_sp[i] / new_spacing[i]))),
+        )
+
+    resample = sitk.ResampleImageFilter()
+    # make the output spacing to match reference spacing
+    resample.SetOutputSpacing(new_spacing)
+    resample.SetSize(out_sz)  # newly calculated size
+    resample.SetOutputDirection(image.GetDirection())
+    resample.SetOutputOrigin(image.GetOrigin())
+    resample.SetTransform(sitk.Transform())
+    resample.SetDefaultPixelValue(image.GetPixelIDValue())
+
+    if is_label:
+        resample.SetInterpolator(sitk.sitkNearestNeighbor)
+    else:
+        resample.SetInterpolator(sitk.sitkLinear)
+
+    return resample.Execute(image)
+
+if __name__ == '__main__':
+  assert 0 == get_saggital_dim('LPS'), 'get_axial_dim() failed'
+  assert 0 == get_saggital_dim('RAI'), 'get_axial_dim() failed'  
+  assert 1 == get_saggital_dim('PRI'), 'get_axial_dim() failed'  
