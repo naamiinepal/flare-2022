5f3c67fa76bf6777bff7668f090ee5877f68c538
mahesh shakya <mahesh.shakya@naamii.org.np>
2022-07-13 13:07:57 +0545

    compare and plot isotropic voxels

--- template_matching/template_matching_utils.py
+++ template_matching/template_matching_utils.py
@@ -42,6 +42,10 @@ def get_longitudinal_dim(orientation:str='LPS'):
   raise ValueError(f'orientation string {orientation} is invalid')
 
 def debug_plot(image_slice:sitk.Image, template_slice:sitk.Image, outTx:sitk.sitkTranslation,transform_list,correlations):
+
+  image_slice = make_isotropic(image_slice)
+  template_slice = make_isotropic(template_slice)
+  
   if image_slice.GetDimension() == 3:
     axial_dim = get_saggital_dim(sitk.DICOMOrientImageFilter_GetOrientationFromDirectionCosines(image_slice.GetDirection()))
     axial_dim_size = image_slice.GetSize()[axial_dim]
@@ -66,8 +70,8 @@ def debug_plot(image_slice:sitk.Image, template_slice:sitk.Image, outTx:sitk.sit
   ax2.add_patch(rect)
 
   ax3 = plt.subplot(1,3,3)
-  ax3.sharey(ax)
-  ax3.plot(correlations,np.array([y for x,y in transform_list]))
+  # ax3.sharey(ax2)
+  ax3.plot(np.array([tx[-1] for tx in transform_list]),correlations)
 
   plt.tight_layout()
   out_filepath = 'template_matching/images/'+generate_random_str()+'.png' 
@@ -82,6 +86,37 @@ def extract_slice_helper(image:sitk.Image,dim=0,index=0):
     slice_index = [0,]*len(size)
     slice_index[dim] = index
     return sitk.Extract(image,size,slice_index)
+
+def make_isotropic(image: sitk.Image,is_label= False):
+    """return a new image whose voxel dimension is isotropic"""
+    ts = (1,)*image.GetDimension()
+    im_sp = image.GetSpacing()
+    new_spacing = ts
+    im_sz = image.GetSize()
+
+    # expand or contract the image size as required
+    out_sz = []
+    for i in range(image.GetDimension()):
+        out_sz.append(
+            int(np.round(im_sz[i] * (im_sp[i] / new_spacing[i]))),
+        )
+
+    resample = sitk.ResampleImageFilter()
+    # make the output spacing isotropic
+
+    resample.SetOutputSpacing(new_spacing)
+    resample.SetSize(out_sz)  # newly calculated size
+    resample.SetOutputDirection(image.GetDirection())
+    resample.SetOutputOrigin(image.GetOrigin())
+    resample.SetTransform(sitk.Transform())
+    resample.SetDefaultPixelValue(image.GetPixelIDValue())
+
+    if is_label:
+        resample.SetInterpolator(sitk.sitkNearestNeighbor)
+    else:
+        resample.SetInterpolator(sitk.sitkLinear)
+
+    return resample.Execute(image)
 
 def match_spacing(reference: sitk.Image, image: sitk.Image, is_label=False):
     """return a new image (with content same as image)
@@ -100,7 +135,8 @@ def match_spacing(reference: sitk.Image, image: sitk.Image, is_label=False):
 
     ts = reference.GetSpacing()
     im_sp = image.GetSpacing()
-    new_spacing = (im_sp[0],im_sp[1],ts[2])
+    # new_spacing = (im_sp[0],im_sp[1],ts[2])
+    new_spacing = ts
     im_sz = image.GetSize()
 
     # expand or contract the image size as required
@@ -127,6 +163,6 @@ def match_spacing(reference: sitk.Image, image: sitk.Image, is_label=False):
     return resample.Execute(image)
 
 if __name__ == '__main__':
-  assert 0 == get_saggital_dim('LPS'), 'get_axial_dim() failed'
-  assert 0 == get_saggital_dim('RAI'), 'get_axial_dim() failed'  
-  assert 1 == get_saggital_dim('PRI'), 'get_axial_dim() failed'  
+  assert 0 == get_saggital_dim('LPS'), 'get_saggital_dim() failed'
+  assert 0 == get_saggital_dim('RAI'), 'get_saggital_dim() failed'  
+  assert 1 == get_saggital_dim('PRI'), 'get_saggital_dim() failed'  
